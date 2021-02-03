7c98f789a8c99ddda17fad0b8703a0de9d01631b
luroy <roylu99@gmail.com>
2021-02-03 00:21:38 -0500

    Add style sheet and darken background

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,11 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
   margin-top: 60px;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+require("./assets/style.css")
 
 createApp(App).mount('#app')
