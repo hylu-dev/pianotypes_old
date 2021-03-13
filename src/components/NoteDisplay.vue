8a99bd832b31962177ef8b372bb68482463e8a1e
luroy <roylu99@gmail.com>
2021-03-13 18:23:10 -0500

    Add NoteDisplay animation;

--- src/components/NoteDisplay.vue
+++ src/components/NoteDisplay.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>{{ currentNote }}</div>
+    <transition name="slide-fade">
+        <div :key="currentNote">{{ currentNote }}</div>
+    </transition>
 </template>
 
 <script>
@@ -22,5 +24,13 @@ export default {
         font-size: 10rem;
         user-select: none;
         color: var(--tertiary-text-colour);
+    }
+    .slide-fade-enter-active, .slide-fade-leave-active {
+        transition: all .8s cubic-bezier(0.19, 1, 0.22, 1);
+    }
+    .slide-fade-enter, .slide-fade-leave-to
+    {
+        opacity: 0;
+        transform: scale(1.1);
     }
 </style>
