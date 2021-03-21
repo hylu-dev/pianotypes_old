4226fe600e7efd62ce6448db4b774c80f7af309f
luroy <roylu99@gmail.com>
2021-03-21 14:45:28 -0400

    Add split mode implementation

--- src/classes/PianoBindings.js
+++ src/classes/PianoBindings.js
@@ -3,41 +3,64 @@ import { Note, Scale } from "@tonaljs/tonal";
 export default class KeyBindingStore {
     constructor(base) {
         this.bindings = {};
+        // normal mode bindings
         this.r1 = ['z','x','c','v','b','n','m',',','.','/'];
         this.r2 = ['a','s','d','f','g','h','j','k','l',';',"'"];
         this.r3 = ['q','w','e','r','t','y','u','i','o','p','[',']'];
         this.r4 = ['1','2','3','4','5','6','7','8','9','0','-','='];
-        this.mode = 0;
+        // split mode bindings
+        this.lr1 = this.r1.slice(0, this.r1.length/2);
+        this.lr2 = this.r2.slice(0, this.r2.length/2);
+        this.lr3 = this.r3.slice(0, this.r3.length/2);
+        this.lr4 = this.r4.slice(0, this.r4.length/2);
+        this.rr1 = this.r1.slice(this.r1.length/2, this.r1.length);
+        this.rr2 = this.r2.slice(this.r2.length/2, this.r2.length);
+        this.rr3 = this.r3.slice(this.r3.length/2, this.r3.length);
+        this.rr4 = this.r4.slice(this.r4.length/2, this.r4.length);
+        this.mode = 2; // 0 for no bindings | 1 for normal mode | 2 for split mode
         this.maxNote = "C9";
         this.base = base;
+        this.splitOffset = 6; // for split mode
         this.createBindings();
+    }
+    /**
+     * 
+     * @param {Array} whiteKeys Array of keys to bind to white keys
+     * @param {Array} blackKeys Array of keys to bind to black keys
+     * @param {Array} scale The scale we want to bind notes to, typically chromatically
+     * @param {Number} index The index of the chromaic scale to start binding from
+     * @returns {Number} The index of the note last binded, can be reused to bind remaining scale
+     */
+    __bindToScale(whiteKeys, blackKeys, scale, index) {
+        let cScale = Scale.rangeOf('C major')(this.base, this.maxNote);
+        let isWhite = (note) => cScale.includes(note) || cScale.includes(Note.enharmonic(note));
+        let indexWhite = 0; // white keys
+        let indexBlack = 0; // black keys
+        if (isWhite(scale[index])) { indexBlack++; } // shift blacks keys 1 if starting on white
+        while (indexWhite<whiteKeys.length && index < scale.length) {
+            let note = scale[index++];
+            isWhite(note) ? this.bindings[whiteKeys[indexWhite++]] = note : this.bindings[blackKeys[indexBlack++]] = note
+            if (isWhite(note) == isWhite(Note.transpose(note,'m2'))) {
+                indexBlack++;
+            }
+        }
+        if (!isWhite(scale[index]) && blackKeys.length > whiteKeys.length && index < scale.length) {  //bind leftover black key if available
+            this.bindings[blackKeys[indexBlack++]] = scale[index]; // don't increment index to allow this key to be doubly bound
+        }
+        return index
     }
     createBindings() {
         this.bindings = {}
-        let cScale = Scale.rangeOf("C major")(this.base, this.maxNote);
-        let chromatic = Scale.rangeOf("C chromatic")(this.base, this.maxNote);
-        let isWhite = (note) => cScale.includes(note) || cScale.includes(Note.enharmonic(note));
-        let index1 = 0; // row1 white keys
-        let index2 = 0; // row2 black keys
-        let index3 = 0; // row3 white keys
-        let index4 = 0; // row4 black keys  
-        let noteIndex = 0;
-        if (isWhite(chromatic[noteIndex])) { index2++; } // shift blacks keys 1 if starting on white
-        while (index1<this.r1.length && noteIndex<chromatic.length) {
-            let note = chromatic[noteIndex++];
-            isWhite(note) ? this.bindings[this.r1[index1++]] = note : this.bindings[this.r2[index2++]] = note
-            if (isWhite(note) == isWhite(Note.transpose(note, "m2"))) {
-                index2+=1;
-            }
-        }
-        if (!isWhite(chromatic[noteIndex])) { this.bindings[this.r2[index2++]] = chromatic[noteIndex]; } //include apostrophe binding if possible
-        if (isWhite(chromatic[noteIndex])) { index4++; } // shift blacks keys 1 if starting on white
-        while (index3<this.r3.length && noteIndex<chromatic.length) {
-            let note = chromatic[noteIndex++];
-            isWhite(note) ? this.bindings[this.r3[index3++]] = note : this.bindings[this.r4[index4++]] = note
-            if (isWhite(note) == isWhite(Note.transpose(note, "m2"))) {
-                index4+=1;
-            }
+        let chromatic = Scale.rangeOf('C chromatic')(this.base, this.maxNote);
+        if (this.mode == 1) {
+            let index = this.__bindToScale(this.r1, this.r2, chromatic, 0);
+            this.__bindToScale(this.r3, this.r4, chromatic, index);
+        } else if (this.mode == 2) {
+            let index = this.__bindToScale(this.lr1, this.lr2, chromatic, 0);
+            index = this.__bindToScale(this.lr3, this.lr4, chromatic, index);
+            index+=this.splitOffset;
+            index =this.__bindToScale(this.rr1, this.rr2, chromatic, index);
+            this.__bindToScale(this.rr3, this.rr4, chromatic, index);
         }
     }
     setMode(mode) {
